05568cf7bbaffe310b1123e5a6b35c5108a69b9b
Emirkcgl <yunus0598koca@gmail.com>
2026-08-12 14:26:48 +0300

    building the  App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayaout from "./UI/AppLayaout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayaout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <div>Hello vite !</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
