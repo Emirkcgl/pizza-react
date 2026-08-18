c256a313305080384f219d8b894b96803f4d6462
Emirkcgl <yunus0598koca@gmail.com>
2026-08-18 13:49:15 +0300

    Fetching Orders

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Form, useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <input placeholder="Search order #" value={query} onChange={(e) => setQuery(e.target.value)} />
+    </Form>
+  );
+}
+
+export default SearchOrder;
