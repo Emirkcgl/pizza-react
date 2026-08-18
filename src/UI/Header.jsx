f7d2d47a339d01258988d23123012bfc2528309f
Emirkcgl <yunus0598koca@gmail.com>
2026-08-18 16:07:41 +0300

    Working With Color

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Hızlı React Pizza</Link>
       <p>Emir</p>
       <SearchOrder />
--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -1,10 +1,10 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-xl text-yellow-500 font-semibold">
+      <h1 className="text-xl font-semibold text-stone-700 text-yellow-500">
         En iyi pizza.
         <br />
-        Fırından çıkar çıkmaz, doğruca size.
+        <span className="text-yellow-500">Fırından çıkar çıkmaz, doğruca size.</span>
       </h1>
     </div>
   );
