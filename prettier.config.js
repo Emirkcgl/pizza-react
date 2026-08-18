7ecd112c6906775b0d77c6e0671fdb2604ce61d4
Emirkcgl <yunus0598koca@gmail.com>
2026-08-18 15:58:33 +0300

    Setting Up Tailwind CSS

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 120,
+};
